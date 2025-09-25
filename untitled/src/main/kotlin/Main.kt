df9ae3631e406e5798592d5c1d109b9ee28e014c
Santiago Pinilla <2132836@cegepmontpetit.ca>
2025-09-24 23:47:29 -0400

    sDvv
    vxvxcvc

---
+++ untitled/src/main/kotlin/Main.kt
@@ -0,0 +1,15 @@
+package org.example
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+fun main() {
+    var somme = 0
+
+    for (i in 3..25){
+        somme= somme + i
+
+    }
+    print( somme)
+
+
+}
